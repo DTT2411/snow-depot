4a8023a9860d687532afa79b703b3e24f5c35c5b
DTT2411 <david.thomson2411@gmail.com>
2025-09-23 16:28:24 +0100

    Added size and length fields to models for specific product subcategories and added size selector to product detail page.

--- products/models.py
+++ products/models.py
@@ -44,6 +44,10 @@ class Product(models.Model):
     product_id = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    has_boot_sizes = models.BooleanField(default=False, null=True, blank=True)
+    has_ski_lengths = models.BooleanField(default=False, null=True, blank=True)
+    has_pole_lengths = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=1, null=True, blank=True
