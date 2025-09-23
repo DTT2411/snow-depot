580f186ba6d1a8ca9fcf78f779190bd5d5182fac
DTT2411 <david.thomson2411@gmail.com>
2025-09-23 01:28:08 +0100

    Fix indentation error in media queries in base.css

--- products/models.py
+++ products/models.py
@@ -11,39 +11,45 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-  
+
     def get_friendly_name(self):
         return self.friendly_name
-    
+
+
 class Subcategory(models.Model):
-    
+
     class Meta:
         verbose_name_plural = 'Subcategories'
-    
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+        )
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-  
+
     def get_friendly_name(self):
         return self.friendly_name
-    
+
+
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+        )
+    subcategory = models.ForeignKey(
+        'Subcategory', null=True, blank=True, on_delete=models.SET_NULL
+        )
     product_id = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=1, null=True, blank=True
+        )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    # clothing Size? XS/S/M/L/XL
-    # Boot size? range between 1 to 14 UK
-    # Ski length? 140/145/150/155/160/165/170/175/180/185/190/195/200
-    # 
 
     def __str__(self):
         return self.name
