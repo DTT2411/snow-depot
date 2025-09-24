559d5a7dcdb081481d0ca3e9b175238121974336
DTT2411 <david.thomson2411@gmail.com>
2025-09-24 01:52:40 +0100

    Fix minor linting errors with Flake8 & started amending product descriptions from placeholder text in django admin.

--- products/models.py
+++ products/models.py
@@ -47,7 +47,9 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     has_boot_sizes = models.BooleanField(default=False, null=True, blank=True)
     has_ski_lengths = models.BooleanField(default=False, null=True, blank=True)
-    has_pole_lengths = models.BooleanField(default=False, null=True, blank=True)
+    has_pole_lengths = models.BooleanField(
+        default=False, null=True, blank=True
+        )
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=1, null=True, blank=True
