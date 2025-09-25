0878561f41c4aecf2d5474902e249e9dc1996f5b
DTT2411 <david.thomson2411@gmail.com>
2025-09-25 18:12:31 +0100

    Prepared checkout view with stripe payment intent and stripe_public_key and client_secret checks.

--- checkout/views.py
+++ checkout/views.py
@@ -9,22 +9,34 @@ import stripe
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Your basket is empty!")
         return redirect(reverse('products'))
 
-    order_form = OrderForm()
     totals = basket_contents(request)
     grand_total = totals['grand_total']
     stripe_total = round(grand_total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing - needs to be set in environment.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51S02vFRxaGtB2eQBXQGgUUBZHU652gy2URNDUCXHBsgV3HvkgyvYgs2jVSW0h3rP88vF14cFdPsKvp5PfYKbE5Mm00TRdfSzcX',
-        # 'grand_total': grand_total,
-        # 'stripe_total': stripe_total,
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
