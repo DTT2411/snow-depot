7fd5dff43282d82ab6e3c7110b343b5bc59a5b2e
DTT2411 <david.thomson2411@gmail.com>
2025-09-25 16:16:30 +0100

    Connect checkout buttons, add order form and order summary to checkout template.

--- snow_depot/urls.py
+++ snow_depot/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('homepage.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('checkout/', include('checkout.urls')),
 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
