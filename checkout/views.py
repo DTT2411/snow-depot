b935f30ab1187420c163c55d5cb8e7a9af8db72c
DTT2411 <david.thomson2411@gmail.com>
2025-09-25 19:11:31 +0100

    Prepare checkout_success view

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from basket.contexts import basket_contents
 
 import stripe
@@ -13,30 +15,131 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "Your basket is empty!")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
 
-    totals = basket_contents(request)
-    grand_total = totals['grand_total']
-    stripe_total = round(grand_total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in basket.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        if item_data.get('items_by_size'):
+                            for size, quantity in item_data['items_by_size'].items():
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    product=product,
+                                    quantity=quantity,
+                                    product_size=size,
+                                )
+                                order_line_item.save()
+                        elif item_data.get('items_by_boot_size'):
+                            for boot_size, quantity in item_data['items_by_boot_size'].items():
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    product=product,
+                                    quantity=quantity,
+                                    product_boot_size=boot_size,
+                                )
+                                order_line_item.save()
+                        elif item_data.get('items_by_ski_length'):
+                            for ski_length, quantity in item_data['items_by_ski_length'].items():
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    product=product,
+                                    quantity=quantity,
+                                    product_ski_length=ski_length,
+                                )
+                                order_line_item.save()
+                        elif item_data.get('items_by_pole_length'):
+                            for pole_length, quantity in item_data['items_by_pole_length'].items():
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    product=product,
+                                    quantity=quantity,
+                                    product_pole_length=pole_length,
+                                )
+                                order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, 'One of the products in your \
+                basket was not found in our database.')
+                    order.delete()
+                    return redirect(reverse('view_basket'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(
+                reverse('checkout_success', args=[order.order_number])
+                )
+        else:
+            messages.error(request, 'There was an error in your form. \
+                Please double check your information.')
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key missing - needs to be set in environment.')
+    else:
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "Your basket is empty!")
+            return redirect(reverse('products'))
 
-    template = 'checkout/checkout.html'
+        totals = basket_contents(request)
+        grand_total = totals['grand_total']
+        stripe_total = round(grand_total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key missing - needs to be set in environment.')
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
+
+
+def checkout_success(request, order_number)
+    """
+    Handles successful checkout
+    """
+    save_info = request.session.get('save_info') # Update later
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order complete! Your \
+        order number is {order_number}. You will receive \
+        a confirmation email at {order.email}.')
+    
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+
     context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'order': order,
     }
 
     return render(request, template, context)
