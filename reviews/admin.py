ec7c48363657d546b52937c5fac9df61fb609b74
DTT2411 <david.thomson2411@gmail.com>
2025-09-27 22:37:26 +0100

    Migrate reviews model and customise review admin in django dashboard.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'user',
+        'created_at',
+    )
+
+
+admin.site.register(Review, ReviewAdmin)
