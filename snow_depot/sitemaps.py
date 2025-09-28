cde72bd1aaf2f04e28987148b4a4cee2f894333a
DTT2411 <david.thomson2411@gmail.com>
2025-09-28 23:19:00 +0100

    Added sitemaps.py and related url, and robot.txt template and related url.

---
+++ snow_depot/sitemaps.py
@@ -0,0 +1,33 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from products.models import Product
+
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # Named URL patterns for top-level static views
+        return [
+            'homepage',
+            'products',
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class ProductSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.6
+
+    def items(self):
+        return Product.objects.all()
+
+    def location(self, obj):
+        return reverse('product_detail', kwargs={'product_id': obj.pk})
+
+    # If you add a last_modified/updated field later, implement lastmod()
+    # def lastmod(self, obj):
+    #     return obj.updated_at
--- snow_depot/urls.py
+++ snow_depot/urls.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
 
 from .views import handler404
+from .sitemaps import StaticViewSitemap, ProductSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'products': ProductSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +22,8 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'snow_depot.views.handler404'
