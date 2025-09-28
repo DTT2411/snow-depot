26496c5f63a46a0b06f3f477c41069bc0df9ce95
DTT2411 <david.thomson2411@gmail.com>
2025-09-28 15:20:07 +0100

    Prepared settings for deployment

--- snow_depot/settings.py
+++ snow_depot/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',  # VS code preview
     'localhost',  # listen for Stripe webhooks
+    'snow-depot-2e7989adac13.herokuapp.com'  # Heroku application
 ]
 
 
@@ -125,16 +127,17 @@ WSGI_APPLICATION = 'snow_depot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse('postgresql://neondb_owner:npg_mJ0djQwfMx1t@ep-young-bush-a28cglni.eu-central-1.aws.neon.tech/easel_set_clink_513225')
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
